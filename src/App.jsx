bb43551bc1b65661ec96443619d2171b11bab9d1
srysis <jashandenis@gmail.com>
2024-09-17 23:39:27 +0200

    added routes for pages related to 'characters database' page

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,17 @@ import Home from "./pages/home"
 import AboutMe from "./pages/about_me"
 import Portfolio from "./pages/portfolio"
 
+import Characters from "./pages/characters/characters"
+import Alice from "./pages/characters/alice"
+import Jane from "./pages/characters/jane"
+import Pandemonica from "./pages/characters/pandemonica"
+import Willow from "./pages/characters/willow"
+
 import "./style/style.css"
 import "./style/style-phone.css"
+
+import "./style/characters/characters.css"
+import "./style/characters/characters-phone.css"
 
 function App() {
 	return (
@@ -19,6 +28,11 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/about_me" element={<AboutMe />} />
 					<Route path="/portfolio" element={<Portfolio />} />
+					<Route path="/characters" element={<Characters />} />
+					<Route path="/characters/alice" element={<Alice />} />
+					<Route path="/characters/jane" element={<Jane />} />
+					<Route path="/characters/pandemonica" element={<Pandemonica />} />
+					<Route path="/characters/willow" element={<Willow />} />
 				</Route>
 			</Routes>
 		</Router>
