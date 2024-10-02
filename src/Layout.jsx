c36a879f7240fd6dcf916d2116264a1f25966e33
srysis <jashandenis@gmail.com>
2024-10-02 22:52:46 +0200

    added Aside component into a Layout render

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,22 @@
+import React from "react"
 import { Outlet } from "react-router-dom"
 
 import Header from "./components/Header"
+import Aside from "./components/Aside"
 import Footer from "./components/Footer"
 
 
 function Layout() {
+	const [isAsideVisible, toggleAsideVisibility] = React.useState(false);
+
+	function toggleAsideVisibilityFromChildren(value) {
+		toggleAsideVisibility(value);
+	}
+
 	return (
 		<>
 			<Header />
+			<Aside isVisible={isAsideVisible} toggleVisibility={toggleAsideVisibilityFromChildren} />
 			<main>
 				<Outlet />
 			</main>
