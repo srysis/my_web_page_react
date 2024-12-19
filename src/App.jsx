4b52904e88688950987c5f9239e10031ad3a12e6
srysis <jashandenis@gmail.com>
2024-12-19 17:28:09 +0100

    removed unused routes; added a 'onBeforeUnload' event function that scrolls the page to top on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Layout from "./Layout"
-
 import Home from "./pages/home"
 import Portfolio from "./pages/portfolio"
 
@@ -11,13 +9,15 @@ import "./style/style.scss"
 import "./style/style-phone.scss"
 
 function App() {
+	window.onbeforeunload = function() {
+		window.scrollTo(0, 0);
+	}
+
 	return (
 		<Router>
 			<Routes>
-				<Route element={<Layout />}>
-					<Route path="/" element={<Home />} />
-					<Route path="/portfolio" element={<Portfolio />} />
-				</Route>
+				<Route path="/" element={<Home />} />
+				<Route path="/portfolio" element={<Portfolio />} />
 			</Routes>
 		</Router>
 	);
