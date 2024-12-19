f761d51f3105999e1b717283f83ffed21decd272
srysis <jashandenis@gmail.com>
2024-12-19 17:22:52 +0100

    major structure overhaul

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,44 +1,29 @@
+import useTitle from "../components/hooks/useTitle.jsx"
 
+import Header from "../components/Header.jsx"
+import Footer from "../components/Footer.jsx"
+
+import AboutMe from "../components/AboutMe.jsx"
+import MoreInfo from "../components/MoreInfo.jsx"
+
+import Portfolio from "../components/PortfolioSection.jsx"
+import Hiring from "../components/Hire.jsx"
 
 function Home() {
+	useTitle("Home");
+
 	return (
 		<>
-			<h2>
-				Welcome to my web-site. On this site I test my skills as well as develop a small portfolio for future employers.
-			</h2>
-			<h3>About me:</h3>
-			<p>
-				My name is Denis. I am a twenty-two year old guy who wants to learn HTML, CSS and JavaScript so I can become a web-developer and turn into a full-time job.
-			</p>
-			<p>
-				I have a bachelor's degree in Software Engineering since I wanted to be a programmer, but as time went on I realized that advanced programming is not my passion, 
-				so I decided to develop myself in Front-End, even though I liked programming and still do as an occasional activity.
-			</p>
-			<p>
-				I have a passion for modelling and animating. I may not be very good at it, but by the time of practicing it, I have learned a lot of things about visual clarity, 
-				simplicity and general principles that can make a picture look good. But even though I really like working with 3D-graphics, 
-				I often find myself burning out and not wanting to do it at all, so I decided to not make it a full-time job, otherwise it would be hard for me to enjoy the thing I really like.
-			</p>
-			<p>
-				I also have an experience with game engines, specifically with Unity. I made a short game as my graduation project, that had one linear level, three types of enemies, 
-				and pretty simple combat and movement mechanics. It taught me a lot about code and project management, as well as modelling and animating basics.
-			</p>
-			<p>
-				That's pretty much all you need to know about my professional life. 
-			</p>
-			<hr />
-			<p>
-				<strong>
-					(ENG) If you are an employer and interested in hiring, you can take a look at my CV 
-						<a href="https://drive.google.com/file/d/1WLmnT231G0Yoj2Cy6v94UlKyBqVN0XBX/view?usp=drive_link" target="_blank"> here</a>.
-				</strong>
-			</p>
-			<p>
-				<strong>
-					(CZ) Pokud jste zaměstnavatel a máte zájem o zaměstnání, můžete se podívat na můj životopis 
-						<a href="https://drive.google.com/file/d/1p2f3MQkbfGnYbNovBmaBHojOpnhvCbF-/view?usp=drive_link" target="_blank"> zde</a>.
-				</strong>
-			</p>
+			<Header />
+			<main>
+				<AboutMe />
+				<MoreInfo />
+				<div id="flex_container">
+					<Portfolio />
+					<Hiring />
+				</div>
+			</main>
+			<Footer />
 		</>
 	)
 }
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,4 +1,6 @@
-import { useNavigate } from "react-router-dom" 
+import { useNavigate } from "react-router-dom"
+
+import useTitle from "../components/hooks/useTitle.jsx"
 
 import characters_image from "../files/portfolio/characters.png"
 import gallery_image from "../files/portfolio/gallery.png"
@@ -13,6 +15,8 @@ import "../style/portfolio/portfolio-phone.scss"
 
 
 function Portfolio() {
+	useTitle("Portfolio");
+
 	const navigate = useNavigate();
 
 	function onClickHandler(site) {
@@ -43,60 +47,58 @@ function Portfolio() {
 
 	return (
 		<>
-			<h1>Portfolio</h1>
-			<h3>
-				Here resides the collection of my little projects that I've managed to build in my learning process. 
-				Some are just implementations of existing designs, while some are the ideas that my made up on my own.
-			</h3>
-			<div className="link_to_project" onClick={() => onClickHandler(6)}>
-				<img src={characters_image} title="Characters" />
-				<div className="text">
-					<h3>Characters</h3>
-					<p>A page where you can read some information about my favorite characters.</p>
+			<h2 id="title">Portfolio</h2>
+			<main>
+				<div className="link_to_project" onClick={() => onClickHandler(6)}>
+					<img src={characters_image} title="Characters" />
+					<div className="text">
+						<h3>Characters</h3>
+						<p>A page where you can read some information about my favorite characters.</p>
+					</div>
 				</div>
-			</div>
-			<div className="link_to_project" onClick={() => onClickHandler(7)}>
-				<img src={gallery_image} title="Gallery" />
-				<div className="text">
-					<h3>Image gallery</h3>
-					<p>A page where you can see images of my favorite characters.</p>
+				<div className="link_to_project" onClick={() => onClickHandler(7)}>
+					<img src={gallery_image} title="Gallery" />
+					<div className="text">
+						<h3>Image gallery</h3>
+						<p>A page where you can see images of my favorite characters.</p>
+					</div>
 				</div>
-			</div>
-			<div className="link_to_project" onClick={() => onClickHandler(5)}>
-				<img src={json_editor_image} title="JSON editor" />
-				<div className="text">
-					<h3>JSON Editor</h3>
-					<p>A simple and very basic JSON editor app.</p>
+				<div className="link_to_project" onClick={() => onClickHandler(5)}>
+					<img src={json_editor_image} title="JSON editor" />
+					<div className="text">
+						<h3>JSON Editor</h3>
+						<p>A simple and very basic JSON editor app.</p>
+					</div>
 				</div>
-			</div>
-			<div className="link_to_project" onClick={() => onClickHandler(1)} >
-				<img src={dart_image} title="DART SERVICE MANAGER" />
-				<div className="text">
-					<h3>DART SERVICE MANAGER</h3>
-					<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+				<div className="link_to_project" onClick={() => onClickHandler(1)} >
+					<img src={dart_image} title="DART SERVICE MANAGER" />
+					<div className="text">
+						<h3>DART SERVICE MANAGER</h3>
+						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+					</div>
 				</div>
-			</div>
-			<div className="link_to_project" onClick={() => onClickHandler(2)} >
-				<img src={corona_image} title="CORONAVIRUS AWARENESS SPREAD" />
-				<div className="text">
-					<h3>CORONAVIRUS AWARENESS SPREAD</h3>
-					<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+				<div className="link_to_project" onClick={() => onClickHandler(2)} >
+					<img src={corona_image} title="CORONAVIRUS AWARENESS SPREAD" />
+					<div className="text">
+						<h3>CORONAVIRUS AWARENESS SPREAD</h3>
+						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+					</div>
 				</div>
-			</div>
-			<div className="link_to_project" onClick={() => onClickHandler(3)}>
-				<img src={marry_house_image} title="MARRY HOUSE" />
-				<div className="text">
-					<h3>MARRY HOUSE</h3>
-					<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+				<div className="link_to_project" onClick={() => onClickHandler(3)}>
+					<img src={marry_house_image} title="MARRY HOUSE" />
+					<div className="text">
+						<h3>MARRY HOUSE</h3>
+						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+					</div>
 				</div>
-			</div>
-			<div className="link_to_project" onClick={() => onClickHandler(4)}>
-				<img src={car_game_image} title="SPEED CAR GAME" />
-				<div className="text">
-					<h3>SPEED CAR GAME</h3>
-					<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+				<div className="link_to_project" onClick={() => onClickHandler(4)}>
+					<img src={car_game_image} title="SPEED CAR GAME" />
+					<div className="text">
+						<h3>SPEED CAR GAME</h3>
+						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+					</div>
 				</div>
-			</div>
+			</main>
 		</>
 	)
 }
