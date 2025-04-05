b8253dfb5a1f35878ed4cb66597405e7df646e17
srysis <jashandenis@gmail.com>
2025-04-05 11:15:39 +0200

    created 'not found page'

---
+++ src/pages/not_found_page.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import "../style/error404.scss"
+
+function NotFoundPage() {
+	return(
+		<div id="error_container">
+			<h1>404 Not Found</h1>
+			<p>Looks like you are lost...</p>
+			<Link to="/">Press here to come back</Link>
+		</div>
+	)
+}
+
+export default NotFoundPage;
