8e4b79b8c93444929389b74585a05931cb12e886
srysis <jashandenis@gmail.com>
2025-05-26 15:53:53 +0200

    created 3 expandable lists each containing several projects, grouped by specific scale/scope; implemented 'expandable list' functionality

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useNavigate } from "react-router-dom"
 
 import useTitle from "../components/hooks/useTitle.jsx"
@@ -49,64 +50,91 @@ function Portfolio() {
 		}
 	}
 
+	function expandList(event) {
+		event.currentTarget.parentElement.classList.toggle("active");
+
+		let list_content = event.currentTarget.nextElementSibling;
+
+		if (list_content.style.maxHeight) {
+			list_content.style.maxHeight = null;
+		} else {
+			list_content.style.maxHeight = list_content.scrollHeight + "px";
+		}
+	}
+
 	return (
 		<>
 			<h2 id="title">Portfolio</h2>
 			<main>
-				<div className="link_to_project" onClick={() => onClickHandler(6)}>
-					<img src={characters_image} title="Characters" />
-					<div className="text">
-						<h3>Characters</h3>
-						<p>A page where you can read some information about my favorite characters.</p>
+				<div id="landing_pages">
+					<button type="button" className="toggle_landing_pages" onClick={expandList}>Landing pages</button>
+					<div className="list_content">
+						<div className="link_to_project" onClick={() => onClickHandler(1)} >
+							<img src={dart_image} title="DART SERVICE MANAGER" />
+							<div className="text">
+								<h3>DART SERVICE MANAGER</h3>
+								<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(2)} >
+							<img src={corona_image} title="CORONAVIRUS AWARENESS SPREAD" />
+							<div className="text">
+								<h3>CORONAVIRUS AWARENESS SPREAD</h3>
+								<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(3)}>
+							<img src={marry_house_image} title="MARRY HOUSE" />
+							<div className="text">
+								<h3>MARRY HOUSE</h3>
+								<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(4)}>
+							<img src={car_game_image} title="SPEED CAR GAME" />
+							<div className="text">
+								<h3>SPEED CAR GAME</h3>
+								<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(7)}>
-					<img src={gallery_image} title="Gallery" />
-					<div className="text">
-						<h3>Image gallery</h3>
-						<p>A page where you can see images of my favorite characters.</p>
+				<div id="full_projects">
+					<button type="button" className="toggle_full_projects" onClick={expandList}>Full projects</button>
+					<div className="list_content">
+						<div className="link_to_project" onClick={() => onClickHandler(8)}>
+							<img src={game_shop_image} title="Game Shop" style={{ "objectPosition": "10% 10%" }} />
+							<div className="text">
+								<h3>Game Shop</h3>
+								<p>A 'proof-of-concept' online shop where games could be bought.</p>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(5)}>
-					<img src={json_editor_image} title="JSON editor" />
-					<div className="text">
-						<h3>JSON Editor</h3>
-						<p>A simple and very basic JSON editor app.</p>
-					</div>
-				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(8)}>
-					<img src={game_shop_image} title="Game Shop" style={{ "objectPosition": "10% 10%" }} />
-					<div className="text">
-						<h3>Game Shop</h3>
-						<p>A 'proof-of-concept' online shop where games could be bought.</p>
-					</div>
-				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(1)} >
-					<img src={dart_image} title="DART SERVICE MANAGER" />
-					<div className="text">
-						<h3>DART SERVICE MANAGER</h3>
-						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
-					</div>
-				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(2)} >
-					<img src={corona_image} title="CORONAVIRUS AWARENESS SPREAD" />
-					<div className="text">
-						<h3>CORONAVIRUS AWARENESS SPREAD</h3>
-						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
-					</div>
-				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(3)}>
-					<img src={marry_house_image} title="MARRY HOUSE" />
-					<div className="text">
-						<h3>MARRY HOUSE</h3>
-						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
-					</div>
-				</div>
-				<div className="link_to_project" onClick={() => onClickHandler(4)}>
-					<img src={car_game_image} title="SPEED CAR GAME" />
-					<div className="text">
-						<h3>SPEED CAR GAME</h3>
-						<p>A landing page template that I have implemented and adapted to smaller screens.</p>
+				<div id="side_projects">
+					<button type="button" className="toggle_side_projects" onClick={expandList}>Side projects</button>
+					<div className="list_content">
+						<div className="link_to_project" onClick={() => onClickHandler(6)}>
+							<img src={characters_image} title="Characters" />
+							<div className="text">
+								<h3>Characters</h3>
+								<p>A page where you can read some information about my favorite characters.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(7)}>
+							<img src={gallery_image} title="Gallery" />
+							<div className="text">
+								<h3>Image gallery</h3>
+								<p>A page where you can see images of my favorite characters.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(5)}>
+							<img src={json_editor_image} title="JSON editor" />
+							<div className="text">
+								<h3>JSON Editor</h3>
+								<p>A simple and very basic JSON editor app.</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</main>
