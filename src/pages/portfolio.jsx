fa84134af70f2f6faf80697a07f53d7a993e8ed1
srysis <jashandenis@gmail.com>
2025-09-16 17:40:59 +0200

    removed 'demos' section; moved 'full projects' section to the top of the projects list; added a link to a new project

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -10,6 +10,7 @@ import marry_house_image from "../files/portfolio/marry_house.png"
 import car_game_image from "../files/portfolio/car_game.png"
 import json_editor_image from "../files/portfolio/json_editor.png"
 import game_shop_image from "../files/portfolio/game_shop.png"
+import forum_image from "../files/portfolio/forum.png"
 
 import "../style/portfolio/portfolio.scss"
 import "../style/portfolio/portfolio-phone.scss"
@@ -44,6 +45,9 @@ function Portfolio() {
 		case 8:
 			window.open("https://srysis-game-shop.netlify.app", "_blank");
 			break;
+		case 9:
+			window.open("https://srysis-forum.netlify.app/", "_blank");
+			break;
 		default:
 			console.error("No such value.");
 			break;
@@ -74,6 +78,56 @@ function Portfolio() {
 		<>
 			<h1 id="title">Portfolio</h1>
 			<main>
+				<div id="full_projects">
+					<button type="button" className="toggle_full_projects" onClick={expandList}>Full projects</button>
+					<div className="list_content">
+						<p className="list_description">
+							This section contains projects, that took relatively longer to make. 
+							These projects have considerably more features, compared to other projects.
+						</p>
+						<div className="link_to_project" onClick={() => onClickHandler(8)}>
+							<img src={game_shop_image} title="Game Shop" alt="Game Shop" style={{ "objectPosition": "10% 10%" }} />
+							<div className="text">
+								<h3>Game Shop</h3>
+								<p>A 'proof-of-concept' online shop.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(9)}>
+							<img src={forum_image} title="Forum" alt="Forum" style={{ "objectPosition": "10% 10%" }} />
+							<div className="text">
+								<h3>"Forum"</h3>
+								<p>A full-stack forum web-application.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="side_projects">
+					<button type="button" className="toggle_side_projects" onClick={expandList}>Side projects</button>
+					<div className="list_content">
+						<p className="list_description">This section contains smaller projects. They don't have a lot of features and usually revolve around one or two functions.</p>
+						<div className="link_to_project" onClick={() => onClickHandler(6)}>
+							<img src={characters_image} title="Characters" alt="Characters" />
+							<div className="text">
+								<h3>Characters</h3>
+								<p>A page where you can read some information about my favorite characters.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(7)}>
+							<img src={gallery_image} title="Gallery" alt="Gallery" />
+							<div className="text">
+								<h3>Image gallery</h3>
+								<p>A page where you can see images of my favorite characters.</p>
+							</div>
+						</div>
+						<div className="link_to_project" onClick={() => onClickHandler(5)}>
+							<img src={json_editor_image} title="JSON editor" alt="JSON editor" />
+							<div className="text">
+								<h3>JSON Editor</h3>
+								<p>A simple and very basic JSON editor app.</p>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div id="landing_pages">
 					<button type="button" className="toggle_landing_pages" onClick={expandList}>Landing pages</button>
 					<div className="list_content">
@@ -107,75 +161,6 @@ function Portfolio() {
 							</div>
 						</div>
 					</div>
-				</div>
-				<div id="full_projects">
-					<button type="button" className="toggle_full_projects" onClick={expandList}>Full projects</button>
-					<div className="list_content">
-						<p className="list_description">
-							This section contains projects, that took relatively longer to make. 
-							These projects have considerably more features, compared to other prjects.
-						</p>
-						<div className="link_to_project" onClick={() => onClickHandler(8)}>
-							<img src={game_shop_image} title="Game Shop" alt="Game Shop" style={{ "objectPosition": "10% 10%" }} />
-							<div className="text">
-								<h3>Game Shop</h3>
-								<p>A 'proof-of-concept' online shop.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div id="side_projects">
-					<button type="button" className="toggle_side_projects" onClick={expandList}>Side projects</button>
-					<div className="list_content">
-						<p className="list_description">This section contains smaller projects. They don't have a lot of features and usually revolve around one or two functions.</p>
-						<div className="link_to_project" onClick={() => onClickHandler(6)}>
-							<img src={characters_image} title="Characters" alt="Characters" />
-							<div className="text">
-								<h3>Characters</h3>
-								<p>A page where you can read some information about my favorite characters.</p>
-							</div>
-						</div>
-						<div className="link_to_project" onClick={() => onClickHandler(7)}>
-							<img src={gallery_image} title="Gallery" alt="Gallery" />
-							<div className="text">
-								<h3>Image gallery</h3>
-								<p>A page where you can see images of my favorite characters.</p>
-							</div>
-						</div>
-						<div className="link_to_project" onClick={() => onClickHandler(5)}>
-							<img src={json_editor_image} title="JSON editor" alt="JSON editor" />
-							<div className="text">
-								<h3>JSON Editor</h3>
-								<p>A simple and very basic JSON editor app.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div id="demos">
-					<button type="button" className="toggle_demos" onClick={expandList}>Project demos</button>
-					<div className="list_content">
-						<p className="list_description">
-							This section contains videos that demonstrate projects, usually full-stack ones. 
-							They were made using all the essential technologies that are required for a Full-Stack application (database, server hosting, handling client-server requests, etc.).
-						</p>
-						<div className="project">
-							<div className="description">
-								<h2>'Game Shop' demo</h2>
-								<p>
-									A build of 'Game Shop' project that was rewritten to utilize database that contains all the neccessary data about games.
-									Uses MySQL as a database, created by phpMyAdmin with MAMP software. As a server, it uses Node.js with Express to communicate with a database.
-									Requests are handled by Axios.
-								</p>
-							</div>
-							<div className="video_container">
-								<video controls controlsList="noRemotePlayback" disablePictureInPicture>
-									<source src="Game_Shop_demo.mp4" type="video/mp4" />
-								</video>
-							</div>
-						</div>
-						
-					</div>
-					
 				</div>
 			</main>
 		</>
